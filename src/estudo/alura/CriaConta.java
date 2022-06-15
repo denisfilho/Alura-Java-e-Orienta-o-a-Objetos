cd5f817d720f6d6ef126f3d3af826bb8d8b33ee0
Denis Ferreira <denisferreira93@hotmail.com>
2022-06-15 11:10:15 -0300

    2 - Vamos la: Orientacao a Objetos - Aula 2: Instanciancao, atributos e referencias

---
+++ src/estudo/alura/CriaConta.java
@@ -0,0 +1,10 @@
+package estudo.alura;
+
+public class CriaConta {
+
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+    }
+}
--- src/estudo/alura/Main.java
+++
@@ -1,8 +0,0 @@
-package estudo.alura;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
