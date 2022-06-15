89cbd7b355d4d74b9dcaeae8b2acd739f1e2dad0
Denis Ferreira <denisferreira93@hotmail.com>
2022-06-15 13:38:31 -0300

    2 - Vamos la: Orientacao a Objetos - Aula 6: Valores default de atributos

--- src/estudo/alura/CriaConta.java
+++ src/estudo/alura/CriaConta.java
@@ -15,5 +15,14 @@ public class CriaConta {
 
         System.out.println("primeira conta tem " + primeiraConta.saldo);
         System.out.println("segunda conta tem " + segundaConta.saldo);
+
+        System.out.println(primeiraConta.agencia);
+        System.out.println(primeiraConta.numero);
+
+        System.out.println(segundaConta.agencia);
+
+        segundaConta.agencia = 146;
+
+        System.out.println("Agora a agência da segunda conta é " + segundaConta.agencia);
     }
 }
