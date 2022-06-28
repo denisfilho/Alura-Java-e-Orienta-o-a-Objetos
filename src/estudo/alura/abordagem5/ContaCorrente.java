a0dc128f9576d9716f4383c1e1fddecea956de10
Denis Ferreira <denisferreira93@hotmail.com>
2022-06-28 15:51:14 -0300

    4 - Heranca e o uso de construtores - Aula 1: Heranca e construtores

---
+++ src/estudo/alura/abordagem5/ContaCorrente.java
@@ -0,0 +1,8 @@
+package estudo.alura.abordagem5;
+
+public class ContaCorrente extends Conta{
+
+    public ContaCorrente(int agencia, int numero){
+        super(agencia, numero);
+    }
+}
---
+++ src/estudo/alura/abordagem5/ContaPoupanca.java
@@ -0,0 +1,8 @@
+package estudo.alura.abordagem5;
+
+public class ContaPoupanca extends Conta{
+
+    public ContaPoupanca(int agencia, int numero){
+        super(agencia, numero);
+    }
+}
