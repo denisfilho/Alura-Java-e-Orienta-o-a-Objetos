81794eda701a911090871472ec5070d67217f773
Denis Ferreira <denisferreira93@hotmail.com>
2022-06-29 13:28:35 -0300

    5 - Classes e metodos abstratos - Aula 3: Metodos abstratos

---
+++ src/estudo/alura/abordagem4/Designer.java
@@ -0,0 +1,8 @@
+package estudo.alura.abordagem4;
+
+public class Designer extends Funcionario{
+    public double getBonificacao(){
+        System.out.println("Chamando o método de bonificacao do DESIGNER");
+        return 200;
+    }
+}
--- src/estudo/alura/abordagem4/EditorVideo.java
+++ src/estudo/alura/abordagem4/EditorVideo.java
@@ -4,7 +4,7 @@ public class EditorVideo extends Funcionario {
 
     public double getBonificacao(){
         System.out.println("Chamando o método de bonificacao do EDITOR DE VIDEO");
-        return super.getBonificacao() + 100;
+        return 150;
     }
 
 }
